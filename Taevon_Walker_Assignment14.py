5ccd2f509330fda4c7c8f2c93f32a8fb2e9f84e8
Taevon Walker <taevonwalker.it@yahoo.com>
2024-08-22 11:19:16 -0400

    Add files via upload

---
+++ Taevon_Walker_Assignment14.py
@@ -0,0 +1,10 @@
+
+
+#input
+def power(base, exponent):
+    return base ** exponent
+
+base = int(input("Enter a base: "))
+exponent = int(input("Enter an exponent: "))
+
+print(power(base, exponent))
